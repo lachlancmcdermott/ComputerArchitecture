d19f3b225ad99991d263354ec56cacbd2655b4b6
Lachlan McDermott <lachlancmcd@gmail.com>
2022-06-18 13:30:01 -0700

    changes

--- BinaryConversion/Program.cs
+++ BinaryConversion/Program.cs
@@ -9,13 +9,21 @@ namespace BinaryConversion
             ADD = 0x10,
             SET = 0x40,
             JMP = 0x42
-        }
+        };
 
-        short[] register = new short[5];
+        const int ADD = 16;
+        const int SET = 64;
+        const int JMP = 66;
+        const int IP = 5;
+
+        public static short[] register = new short[6];
+        public static int[] instructions = new int[6];
+
+        public static int loop = 5;
 
         static bool IsPowerOfTwo(int number)
         {
-            if (number >>1 == 1)
+            if (number >> 1 == 1)
             {
                 return true;
             }
@@ -49,7 +57,6 @@ namespace BinaryConversion
             end:
             return number;
         }
-
         static bool IsNthBitOne(int number, int bitIndex)
         {
             int mask = 1 << bitIndex;
@@ -61,43 +68,89 @@ namespace BinaryConversion
             return (byte)(number & 0b_1111_1111);
         }
 
-        static void DoWork(int instruction, short[] register)
+        static void DoWork()
         {
-            byte OpCode = GetNthByte(instruction, 3);
-            int b1 = GetNthByte(instruction, 2);
-            int b2 = GetNthByte(instruction, 1);
-            byte b3 = GetNthByte(instruction, 0);
-            if (OpCode == 64) //SET - 0x40_Register#_FF_Value to store in register
+            int cI = register[IP];
+
+            byte OpCode = GetNthByte(instructions[cI], 3);
+            byte b1 = GetNthByte(instructions[cI], 2);
+            byte b2 = GetNthByte(instructions[cI], 1);
+            byte b3 = GetNthByte(instructions[cI], 0);
+
+            if (OpCode == 0x40) //SET
             {
                 register[b1] = b3;
+                register[IP]++;
             }
-            else if(OpCode == 16) //ADD - 0x10_Register to add, register to add, first register (aka. sum)
+            else if (OpCode == 0x10) //ADD
             {
-                register[b1] = register[b1] + register[b2];
+                register[b3] = (short)(register[b1] + register[b2]);
+                register[IP]++;
             }
-            else if(OpCode == 66) //JMP - 0x42 
+            else if (OpCode == 0x42) //JMP
             {
-                //????
+                register[IP] = b1;
+            }
+            else if (OpCode == 0x31) //JMPT
+            {
+                if (b1 < b3)
+                {
+                    //JMP
+                    register[IP] = b2;
+                }
+                else
+                {
+                    //cont
+                    register[IP]++;
+                }
             }
         }
 
+        /// Set R0 0; count = 0
+        /// Set R1 1; inc = 1
+        /// Set R25; limit = 5
+        /// LOOP:
+        /// LT R3 R0 R2; count to limit
+        /// JMPZ end R3
+        /// ADD R0 R1 R0; count++;
+        /// JMP loop end;
 
         static void Main(string[] args)
         {
-            short[] register = new short[5];
-            PrintRegister(register);
-            Console.WriteLine();
-            DoWork(0x4001FF01, register);
-            PrintRegister(register);
+            instructions[0] = 0x4000FF01;
+            instructions[1] = 0x4001FF01;
+            instructions[2] = 0x4002FF05;
+            instructions[3] = 0x10000100;
+            //JMP
+            //instructions[4] = 0x4202FFFF;
+            //JMPT
+            instructions[4] = 0x31000203;
+
+
+            DoWork();
+            PrintRegister();
         }
-        
-        static void PrintRegister(short[] register)
+
+        static void PrintRegister()
         {
             for (int i = 0; i < register.Length; i++)
             {
                 Console.WriteLine(register[i]);
             }
+            Console.WriteLine();
         }
+
+
+
+
+        /// <summary>
+        /// Instructions
+        /// </summary>
+        /// Math 0x10 (ADD, SUB, MUL, DIV, MOD, SHL, SHR)
+        /// Logic 0x20 (AND, OR, XOR, NOT, GTE, LTE, GT, LT, EQ)
+        /// Flow Control 0x30 (JMP, JMPT (maybe JMPZ))
+        /// Memory 0x40 (SET, COPY (MOV), LOAD, STORE, PUSH, POP)
+        /// Category, ASM, OpCode, Layout (OpCode, posA, posB, posC), Description, Example (english input and hex example)
 
     }
 }
